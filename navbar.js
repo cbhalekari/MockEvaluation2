cc2cb1ac2bf6297e91c13416cedda43a4dfa6dd5
cbhalekari <chaitalib1262@gmail.com>
2024-12-26 10:44:30 +0000

    code for navbar javascript

---
+++ navbar.js
@@ -0,0 +1,7 @@
+document.getElementById('navbar').innerHTML = `
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="quiz.html">Quiz</a>
+        <a href="questions.html">Questions</a>
+    </nav>
+`;
